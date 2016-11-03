a6bcda2a9b2dc8977e26b7084f232fa6560c011e
chenjeffrey23 <chenjeffrey23@gmail.com>
2016-11-03 16:41:23 -0700

    more stuff

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function main() {
+  $('#accomplishments').hide();
+  $('#accomplishmentsbutton').on('click',function(){
+  $(this).next().slideToggle(350);
+  $(this).toggleClass('.aactive');
+  });
+}
+
+$(document).ready(main);
