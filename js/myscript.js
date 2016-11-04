1e65a7f86e9fd495ab0e6aeb7cd65246551292b6
Jeffrey Chen <chenjeffrey23@gmail.com>
2016-11-03 23:05:19 -0700

    More Stuff

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,25 @@
 function main() {
+  $("#indexpic1").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
+    $("#biopic").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
+  $("#resumepic").fancybox({
+          helpers: {
+              title : {
+                  type : 'float'
+              }
+          }
+      });
   $('#accomplishments').hide();
   $('#accomplishmentsbutton').on('click',function(){
   $(this).next().slideToggle(350);
